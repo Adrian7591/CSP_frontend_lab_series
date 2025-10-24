3f646064c989eee8f992675a807f7beb85128e90
Adrian7591 <aquinones-sordo26370@columbushs.com>
2025-10-24 14:00:18 -0400

    Finished 1.1

--- Units/Unit 1 - JavaScript Basics/1.1 - JavaScript Intro/Student/Worksheets/GuidedPractice.md
+++ Units/Unit 1 - JavaScript Basics/1.1 - JavaScript Intro/Student/Worksheets/GuidedPractice.md
@@ -1,12 +1,16 @@
 # Guided Practice — Lab 1.1 (Intro)
 
-1) Predict the output:
+1) Predict the output: 55 because you have a number and a string 
 ```js
 let x = 5;
 let y = "5";
 console.log(x + y);
 ```
 Explain why that output occurs.
+
+let a ="7";
+let b = "3";
+console.log (Number (a) + Number (b))
 
 2) Fix the bug so each line logs a number, not a string:
 ```js
--- Units/Unit 1 - JavaScript Basics/1.1 - JavaScript Intro/Student/src/Lab1_1_JSIntro.js
+++ Units/Unit 1 - JavaScript Basics/1.1 - JavaScript Intro/Student/src/Lab1_1_JSIntro.js
@@ -4,27 +4,63 @@
    =========================================================== */
 
 // TODO 1: Print your full name with console.log()
-
+console.log("Adrian Quinones-Sordo");
 
 // TODO 2: Declare variable favoriteFood, assign "Steak", then reassign "Pizza"
-
+let favoriteFood = "Steak";
+console.log(favoriteFood);
+favoriteFood = "Pizza";
+console.log(favoriteFood);
 
 // TODO 3: Create variable mystery, assign a string, then a number, print type each time
+let mystery = "Hello";
+console.log ("Value: " + mystery + "Data Type: "+ typeof mystery);
+mystery = 24;
+console.log ("Value: " + mystery + "Data Type: "+ typeof mystery)
+
 
 
 // TODO 4: Declare const schoolName, try to reassign
-
+const schoolName = "Christopher Columbus High School";
+// schoolName = "Belen"; cannot reasign value to const
 
 // TODO 5: Create three valid camelCase variables and print them
-
+let firstName; 
+let lastName; 
+let birthYear; 
+let favoriteCity;
+let carType;
+console.log (firstName)
+console.log (lastName)
+console.log (birthYear)
+console.log (favoriteCity)
+console.log (carType)
 
 // 🚀 Stretch Goals (optional)
 // - Create a variable age and print "I am X years old" using concatenation
 // - Repeat using template literals
+let age = 16;
+console.log(`I am ${age} years old`); // template literals 
+console.log("I am" + age + " years old"); // string concatenation
 
 /* ===========================================================
    🎟 EXIT TICKET
-   1. What new concept or skill did you learn in this lab?
+   1. What new concept or skill did you learn in this lab? 
+      let, string, const, typeof, number
    2. What challenges did you encounter, and how did you solve them?
-   3. Where else could this programming concept be useful?
+      I got an error and went back into the console to find the line and fix it 
+   3. Where else could this programming concept be useful? 
+   It could be applied anywhere
    =========================================================== */
+
+let a ="7";
+let b = "3";
+console.log (Number (a) + Number (b))
+
+let width = 5;
+let height = 4;
+let area = width * height;
+let perimeter = 2 * (width + height);
+console.log (`The area of the rectangle is ${area}`)
+console.log (`The perimeter of the rectangle is ${perimeter}`)
+
